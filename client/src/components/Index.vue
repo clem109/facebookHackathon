e4fccd9389b47149bd396c501e6eb01a781306a7
clement venard <cvenard@me.com>
2017-04-14 11:30:58 +0800

    moved client code into client folder and created server folder

---
+++ client/src/components/Index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <router-link to="/order">
+    <button>Click Me</button>
+    <p> TODO: Add in a homescreen page
+    {{ hello }}</p>
+  </router-link>
+</div>
+</template>
+
+<script>
+
+import ItemCard from './ItemCard.vue'
+import gql from 'graphql-tag'
+
+export default {
+
+  data () {
+    return {
+      hello: ''
+    }
+  },
+  components: {
+    ItemCard
+  },
+  apollo: {
+  // Simple query that will update the 'hello' vue property
+  hello: gql`{
+    id
+  }`,
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+.list {padding-top: 20px;}
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,42 @@
+// === DEFAULT / CUSTOM STYLE ===
+// WARNING! always comment out ONE of the two require() calls below.
+// 1. use next line to activate CUSTOM STYLE (./src/themes)
+// require(`./themes/app.${__THEME}.styl`)
+// 2. or, use next line to activate DEFAULT QUASAR STYLE
+require(`quasar/dist/quasar.${__THEME}.css`)
+// ==============================
+
+import Vue from 'vue'
+import Quasar from 'quasar'
+import router from './router'
+import { store } from './store/store'
+import { ApolloClient, createNetworkInterface } from 'apollo-client'
+import VueApollo from 'vue-apollo'
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'https://api.github.com/users/clem109',
+    transportBatching: true
+  }),
+  connectToDevTools: true
+})
+
+// Install the vue plugin
+Vue.use(VueApollo)
+Vue.use(Quasar) // Install Quasar Framework
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
+Quasar.start(() => {
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#q-app',
+    router,
+    store,
+    apolloProvider,
+    render: h => h(require('./App'))
+  })
+})
